4a13b4830e3ece2f4403d7e759ccafc01a0c2d44
Dinesh <rekha0482@gmail.com>
2015-12-16 19:44:06 +0530

    update contact us page

---
+++ js/form-validation.js
@@ -0,0 +1,53 @@
+
+
+$(document).ready(function() {
+    
+    // Setup form validation on the #account-setting element
+    $("#callus").validate({
+        // Specify the validation rules
+        rules: {
+            name: {
+             required: true
+            },
+            email: {
+                required: true,
+				email: true
+            },
+			subject: {
+                required: true
+            },
+			message: {
+                required: true
+            }
+           
+        },
+        
+        // Specify the validation error messages
+        messages: {
+            name:  {
+				required: "Please enter your full name" },
+             email: {
+                required: "Please enter email",
+				email: "Please enter valid email address"
+                
+            },
+			subject: {
+                required: "Please enter subject "
+            },
+			message: {
+                required: "Please Enter message"
+            }
+           
+        },
+        
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+	
+	
+	
+ 
+  });/*end document ready*/
+  
+ 
